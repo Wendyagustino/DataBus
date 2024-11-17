d3bcb3aead2a3147eb2be627d634e2a983234d89
Wendyagustino <wendy23ti@mahasiswa.pcr.ac.id>
2024-11-17 15:51:15 +0700

    User Wendy

--- kel2/resources/views/user/wendy.blade.php
+++ kel2/resources/views/user/wendy.blade.php
@@ -1,5 +1,12 @@
 @extends('layout.app')
 @section('content')
 
-<h1>Halo</h1>
+<h1>Informasi Wendy</h1>
+
+<p>Nama : Wendy Agustino</p><br>
+<p>Email : Wendy23ti@mahasiswa.pcr.ac.id </p><br>
+<p>Kelas : 2 TID </p><br>
+
+<p><b>Senyuman mungkin terlihat sederhana, tapi memiliki kekuatan untuk mengubah hari seseorang, termasuk hari Anda sendiri! </b></p><br>
+
 @endsection
