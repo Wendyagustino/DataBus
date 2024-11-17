c14bded947687d93a27c73164bc08a4d7c3e9162
MiftahulRizki21 <miftahulrizki204@gmail.com>
2024-11-17 15:25:52 +0700

    route

--- kel2/routes/web.php
+++ kel2/routes/web.php
@@ -8,3 +8,12 @@ Route::get('/', function () {
 Route::get('/adib', function () {
     return view('user.adib');
 });
+Route::get('/rizki', function () {
+    return view('user.rizki');
+});
+Route::get('/wendy', function () {
+    return view('user.wendy');
+});
+Route::get('/zahwa', function () {
+    return view('user.zahwa');
+});
